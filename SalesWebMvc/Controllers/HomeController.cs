c0d224009f3083d49924de1f72a9e7c34aa43ac4
Danilo Shinki <daniloshinki@live.com>
2021-04-17 20:26:09 -0300

    controller and view about

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["email"] = "daniloshinki@live.com";
+            ViewData["Aluno"] = "Danilo Shinki Yoshizato";
 
             return View();
         }
